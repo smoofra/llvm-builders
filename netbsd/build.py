39bb24fb9b26db2eaa8d40d92b85927592f5d415
Lawrence D'Anna <larry@elder-gods.org>
2019-10-25 11:06:48 -0700

    gcloud

--- netbsd/build.py
+++ netbsd/build.py
@@ -2,14 +2,24 @@
 
 # some code copied from https://github.com/google/netbsd-gce
 
+import subprocess
 import ftplib
+import os
 import sys
+import argparse
 
 import pexpect.exceptions
 
 # this doesn't appear to be on pypi
 # https://github.com/gson1703/anita
 import anita
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--key")
+parser.add_argument("--google-compute-engine", "--gce", action='store_true')
+args = parser.parse_args()
+
 
 def find_latest_release(branch, arch):
   """Find the latest NetBSD-current release for the given arch.
@@ -33,16 +43,23 @@ def find_latest_release(branch, arch):
 arch = 'amd64'
 branch = 'netbsd-8'
 disk_size = '24G'
-pkg_path = "http://cdn.NetBSD.org/pub/pkgsrc/packages/NetBSD/amd64/8.1/All/"
-pkgs = "cmake swig3 curl python37 vim ccache ninja-build mozilla-rootcerts git"
+version = '8.1'
+pkg_path = f"http://cdn.NetBSD.org/pub/pkgsrc/packages/NetBSD/{arch}/{version}/All/"
+pkgs = "cmake swig3 curl python37 vim ccache ninja-build mozilla-rootcerts git py37-pip"
+workdir = f"work-{branch}-{arch}"
 
-workdir = "work-%s-%s" % (branch, arch)
+if args.google_compute_engine:
+  out = subprocess.run(['tar', '--version'], stdout=subprocess.PIPE).stdout
+  if b'GNU tar' in out:
+    tar = 'tar'
+  else:
+    tar = 'gtar'
 
 a = anita.Anita(
     anita.URL(find_latest_release(branch, arch)),
     workdir=workdir,
     disk_size=disk_size,
-    memory_size="4G",
+    memory_size="16G",
     persist=True)
 
 
@@ -59,30 +76,64 @@ def boot_and_run(commands):
   except pexpect.exceptions.EOF:
     pass
 
-boot_and_run([
-    "grep -q sshd /etc/rc.conf || echo sshd=YES >>/etc/rc.conf",
+commands = [
     "grep -q dhcpcd /etc/rc.conf || echo dhcpcd=YES >>/etc/rc.conf",
     "grep -q ipv4only /etc/dhcpcd.conf || echo ipv4only >> /etc/dhcpcd.conf",
-])
+]
+
+if args.key:
+  with open(args.key, 'r') as f:
+    key = f.read().strip()
+
+  commands += [
+    "grep -q sshd /etc/rc.conf || echo sshd=YES >>/etc/rc.conf",
+    'echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config',
+    'mkdir -p .ssh',
+    'chmod 0700 .ssh',
+    f"echo '{key}' >>.ssh/authorized_keys",
+    'chmod 0600 .ssh/authorized_keys',
+  ]
+
+boot_and_run(commands)
 
 boot_and_run([
     f'env PKG_PATH="{pkg_path}" pkg_add {pkgs}',
     "mozilla-rootcerts install",
 ])
 
-boot_and_run(['true'])
+gce_commands = [
+    """cat > /etc/ifconfig.vioif0 << EOF
+!dhcpcd vioif0
+mtu 1460
+EOF""",
+    """ed /etc/fstab << EOF
+H
+%s/wd0/sd0/
+wq
+EOF""",
+]
+
+if args.google_compute_engine:
+  boot_and_run(gce_commands)
+
+  print("finished with qemu, tarring up for google compute engine ")
+
+  tarball =  f'netbsd-{version}-gce.tar.gz'
+  subprocess.run([tar, '-Szcf', tarball, '--transform', f's,{workdir}/wd0.img,disk.raw,', f'{workdir}/wd0.img'],
+    check=True)
+  print("tarball created:", tarball)
 
 print("DONE.")
-print()
-print(f"""
-To run the VM:
 
-qemu-system-x86_64 -m 4096 -drive file={workdir}/wd0.img,format=raw,media=disk,snapshot=off -nographic -nic user,hostfwd=tcp::2222-:22
+if not args.google_compute_engine:
+  print()
+  print(f"""
+  To run the VM:
+  qemu-system-x86_64 -m 16384 -drive file={workdir}/wd0.img,format=raw,media=disk,snapshot=off -nographic -nic user,hostfwd=tcp::2222-:22
 
-To convert the image:
+  To convert the image:
+  qemu-img convert -f raw  {workdir}/wd0.img  -O qcow2 netbsd-8.1.qcow
 
-qemu-img convert -f raw  {workdir}/wd0.img  -O qcow2 netbsd-8.1.qcow
-
-""")
+  """)
 
 sys.exit(0)
